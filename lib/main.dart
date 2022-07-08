799a62685a8ebf68b69eb0e617191425c6004975
Avighna Jha <avighnajha1703@gmail.com>
2022-07-08 19:41:02 +0530

    Now using a dictionary to store questions and answers and all buttons have realistic text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,18 +14,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex += 1;
+      _questionIndex += 1;
     });
-    print("Answer Chosen");
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = ["Favorite color?", "Favorite animal?"];
+    var questions = [
+      {
+        "questionText": "Favorite color?",
+        "answers": ["Black", "Red", "Green", "White"]
+      },
+      {
+        "questionText": "Favorite animal?",
+        "answers": ["Rabbit", "Snake", "Elephant", "Lion"]
+      },
+      {
+        "questionText": "Favorite sport?",
+        "answers": ["Cricket", "Football", "Tennis", "Beyblade"]
+      }
+    ];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
@@ -32,10 +45,11 @@ class _MyAppState extends State<MyApp> {
       ),
       body: Column(
         children: [
-          Question(questions[questionIndex]),
-          RaisedButton(child: Text("Answer 1"), onPressed: answerQuestion),
-          RaisedButton(child: Text("Answer 2"), onPressed: answerQuestion),
-          RaisedButton(child: Text("Answer 3"), onPressed: answerQuestion)
+          Question(questions[_questionIndex]["questionText"] as String),
+          ...(questions[_questionIndex]["answers"] as List<String>)
+              .map((answer) {
+            return Answer(_answerQuestion, answer);
+          }).toList()
         ],
       ),
     ));
